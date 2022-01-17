21d27609f170e7f6e7bef0f87d1b39a97e083901
edgar83628 <edgar.83628@gmail.com>
2022-01-16 18:20:03 -0600

    Add files via upload
    My attempt at a google site

---
+++ MyWebsite/register/index.php
@@ -0,0 +1,18 @@
+
+</!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Sign Up</title>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+  <div class="wrapper">
+    <h2>Sign Up</h2>
+    <p>Please fill this form to create an account.</p>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" </form>
+    
+  </div>
+
+</body>
+</html>
